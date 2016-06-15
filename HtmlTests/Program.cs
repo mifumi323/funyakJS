c3758a8c2384529b12135203f499a12d3ebb41a4
mifumi323 <mifumi323@gmail.com>
2016-06-15 22:22:36 +0900

    HtmlView HtmlCore
    事情があってコミットをまとめた。

---
+++ HtmlTests/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using DuoCode.Dom;
+//using static DuoCode.Dom.Global; // uncomment to use C# 6.0 'using static' syntax
+
+namespace HtmlTests
+{
+    static class Program
+    {
+        static void Run() // HTML body.onload event entry point, see index.html
+        {
+            System.Console.WriteLine("Hello funyak");
+        }
+    }
+}
